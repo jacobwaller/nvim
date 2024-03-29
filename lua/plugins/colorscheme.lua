2183e432d2c295e6f609d177e0ed64464a178bef
Jacob Waller <jacobwaller@jacobwaller.com>
2024-03-29 15:51:00 -0500

    change theme:

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,3 +1,20 @@
+-- Using lazy.nvim
+if true then
+  return
+  -- Using lazy.nvim
+  {
+    "ribru17/bamboo.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("bamboo").setup({
+        -- optional configuration here
+      })
+      require("bamboo").load()
+    end,
+  }
+end
+
 return {
   -- add gruvbox
   { "ellisonleao/gruvbox.nvim" },
